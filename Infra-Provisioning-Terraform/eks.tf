565f507f3a7125dd1f8e7d84a5bf7dce4c0c6818
Gaurav-Sadhwani1 <67726684+Gaurav-Sadhwani1@users.noreply.github.com>
2024-11-08 17:56:05 +0530

    Create eks.tf

---
+++ Infra-Provisioning-Terraform/eks.tf
@@ -0,0 +1,33 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.name
+  cluster_version = "1.31"
+  cluster_endpoint_public_access  = true
+  enable_cluster_creator_admin_permissions = true
+  # EKS Addons
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  create_cloudwatch_log_group = false
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2_x86_64"
+      instance_types = ["m6i.large"]
+
+      min_size = 1
+      max_size = 1
+      desired_size = 1
+    }
+  }
+
+  tags = local.tags
+}
